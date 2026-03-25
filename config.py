3e7ec247e9e2ed11749e4f4d74dcce8c486a76cd
Ebenzic-lang <adeboyeebenezer2000@gmail.com>
2026-03-25 15:38:08 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "8749400865:AAF_aUMc6zIFUZvGARNfTnmMgqnz_OQ-xGo"
+CHANNEL_USERNAME = "@Palmernews"
+
+PLAY_LINK = "https://t.me/gemzcoin_bot/playfun?startapp=eyJzIjoidGVsZWdyYW0iLCJtIjoiYm90IiwiZiI6ImJyb2FkY2FzdCJ9"
+OFFER_LINK = "https://t.me/Bybitglobal_Official_bot/app?startapp=redirect-Wednesday-Airdrop"
+SUPPORT_LINK = "https://t.me/Vipwinningsignals"
